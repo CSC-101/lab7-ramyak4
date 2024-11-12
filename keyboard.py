1ff96095345905ddbb6ec873fda73ceaabf2b0c3
Ramya Kaja <ramyasree.kaja@gmail.com>
2024-11-11 21:02:46 -0800

    Submitted Lab 7

--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,17 @@
+from convert import str_to_float
 
+
+# Task 2
+# This function
+def gather_numbers() -> list[float]:
+    nums = []
+    while True:
+        value = input("Enter a number")
+        if value.lower() == 'done':
+           break
+        if str_to_float(value) is not None:
+            nums.append(str_to_float(value))
+    return nums
+
+if __name__ == '__main__':
+    print("Sum of numbers:", sum(gather_numbers()))
